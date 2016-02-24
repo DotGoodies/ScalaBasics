bda92c244900c5be8bad968b38f1c0ee8d084394
Igor <igor@Ubuntu1>
2016-02-25 00:13:07 +0100

    managed to make elements example working

--- src/compositionAndInheritance/Element.scala
+++ src/compositionAndInheritance/Element.scala
@@ -1,47 +1,82 @@
 package compositionAndInheritance
 
-/**
-  * Created by igor on 22-2-16.
-  */
-abstract class Element {
-  def contents : Array[String]
-  val height = contents.length
-  val width =
-    if(height == 0) 0 else contents(0).length
+object Element{
+  def elem(contents : Array[String]) : Element = new ArrayElement(contents)
+  def elem(value: String) : Element = new LineElement(value)
+  def elem(ch: Char, width : Int, height : Int): Element =
+    new UniformElement(ch, width, height)
 
-  def above(other : Element) =
-    new ArrayElement(contents ++ other.contents)
+  def array(value: String*): Element = new ArrayElement(value.toArray)
 
-  def beside(other: Element) = {
-    val contents = new Array[String](this.contents.length)
-    for(i <- 0 until this.contents.length)
-      contents(i) = this.contents(i) + other.contents(i)
+  def blank(width : Int, height : Int) : Element = new UniformElement(' ', width, height)
 
-    new ArrayElement(contents)
+  private class ArrayElement(val contents : Array[String]) extends Element
+
+  private class LineElement
+  (
+    val value: String
+  ) extends Element{
+    val contents = Array(value)
+    override val width = contents.length
+    override val height = 1
+  }
+
+  private class UniformElement
+  (
+    val ch: Char,
+    override val width : Int,
+    override val height : Int
+  ) extends Element{
+    def contents = Array.fill(height){
+      ch.toString * width
+    }
   }
 }
 
-class ArrayElement_(elements : Array[String]) extends Element{
-  def contents = elements
+import Element.elem
+import Element.blank
+
+abstract class Element {
+  def contents: Array[String]
+
+  def width =
+    if(height == 0) 0 else contents(0).length
+  def height = contents.length
+
+  def widen(w: Int): Element =
+    if (w <= width) this
+    else {
+      val left = blank((w - width) / 2, height)
+      val right = blank(w - width - left.width, height)
+
+      left beside this beside right
+    }
+
+  def heighten(h: Int): Element =
+    if (h <= height) this
+    else {
+      val top = blank(width, (h - height) / 2)
+      val bottom = blank(width, h - height - top.height)
+
+      top above this above bottom
+    }
+
+  def above(other: Element) = {
+    val this1 = this widen other.width
+    val other1 = other widen this.width
+    elem(this1.contents ++ other1.contents)
+  }
+
+  def beside(other: Element) = {
+
+    val this1 = this heighten other.height
+    val other1 = other heighten this.height
+
+    elem(
+      for ((left, right) <- this1.contents zip other1.contents)
+        yield left + right
+    )
+  }
+  override def toString = contents mkString "\n"
 }
 
-class ArrayElement(val contents : Array[String]) extends Element
-
-class LineElement
-(
-  val value: String
-) extends Element{
-  val contents = Array(value)
-  override val width = contents.length
-  override val height = 1
-}
-
-class UniformElement
-(
-  val ch: Char,
-  override val width : Int,
-  override val height : Int
-) extends Element{
-  private val line = ch.toString * width
-  def contents = Array.fill(height)(line)
-}
--- src/compositionAndInheritance/ElementEntryPoint.scala
+++ src/compositionAndInheritance/ElementEntryPoint.scala
@@ -3,8 +3,13 @@ package compositionAndInheritance
 /**
   * Created by igor on 22-2-16.
   */
-object ElementEntryPoint extends App {
-  val ae = new ArrayElement(Array("abc", "edrfz"))
 
-  println(ae.width)
+import Element.array
+
+object ElementEntryPoint extends App {
+  val x1 = array("Hello", "z") above array("world!", "q")
+  val x2 = array("one", "two") beside array("one", "x")
+
+  println(x1)
+  println(x2)
 }
